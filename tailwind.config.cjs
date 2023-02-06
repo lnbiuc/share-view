50a64608393a951a759e4fcb3f681b4fc527fa81
lnbiuc <15771670073@163.com>
2023-02-06 17:59:56 +0800

    feat: supplement

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
-  theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    content: [],
+    theme: {
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        fontFamily: {
+            sans: ["Graphik", "sans-serif"],
+            serif: ["Merriweather", "serif"],
+        },
+        extend: {
+            spacing: {
+                128: "32rem",
+                144: "36rem",
+            },
+            borderRadius: {
+                "4xl": "2rem",
+            },
+        },
     },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
-  },
-  plugins: [],
-}
+    plugins: [],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server:{
-    proxy: {
-      // 解除跨域写法
-      '/api': {
-        target: 'https://share.vio.vin/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
+    plugins: [vue()],
+    server: {
+        proxy: {
+            // 解除跨域写法
+            "/api": {
+                target: "https://share.vio.vin/api",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
 });
