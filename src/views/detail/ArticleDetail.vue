30c68e6e8d092574e8d001d3ca75bec5d53f2bdd
lnbiuc <15771670073@163.com>
2023-02-13 18:55:30 +0800

    feat:

--- src/views/detail/ArticleDetail.vue
+++ src/views/detail/ArticleDetail.vue
@@ -5,60 +5,8 @@ import { ElMessage } from 'element-plus';
 import { useRouter } from 'vue-router';
 
 const articleId = useRouteParams<string>('articleId');
-const author = ref<UserEntity>({
-    userId:"978SdfDS",
-    username:"lnbiuc",
-    phone:"17743931264",
-    mail:"13251@432.com",
-    signature:"wsssd",
-    avatar:"null",
-    level:2,
-    isBlock:false,
-    permissionLevel:2,
-    registerTime:"2023-01-28 13:32:26",
-    isMailNotice:true,
-    isPhoneNotice:true,
-    theme:"null",
-    lastLogin:"2023-01-28 13:32:32",
-    loginIp:"127.0.0.1",
-    ipAddr:"null"
-});
-const article = ref<ArticleEntity>({
-    articleId:"NpS4Dt",
-    author: author.value,
-    title:"Article_title3-test",
-    introduction:"Article_introduction3-test",
-    type:"Article",
-    tags:[
-        {
-            tagId:2001,
-            tagName:"Tags-1"
-        },
-        {
-            tagId:2002,
-            tagName:"Tags-2"
-        },
-        {
-            tagId:2003,
-            tagName:"Tags-3"
-        },
-        {
-            tagId:2010,
-            tagName:"Tags-10"
-        },
-        {
-            tagId:2010,
-            tagName:"Tags-10"
-        }
-    ],
-    category:"C_4-test",
-    releaseTime:"2024-01-28 17:36:09",
-    lastUpdate:"2023-01-28 20:36:10",
-    setTop:false,
-    views:48,
-    like:10,
-    comments:6
-});
+const author = ref<UserEntity>();
+const article = ref<ArticleEntity>();
 const comments = ref<CommentListEntity>();
 
 getOneArticle(articleId.value).then((res) => {
@@ -80,7 +28,32 @@ getOneArticle(articleId.value).then((res) => {
         </div>
         <div class="flex flex-col rounded-sm hover:shadow-md bg-purple-200 sm:max-w-screen-sm md:max-w-screen-md ls:max-w-screen-ls lg:max-w-4xl">
             <div class="flex flex-col">
-                article
+                <div>
+                    <span>{{ article.title }}</span>
+                    <span>{{ article.introduction }}</span>
+                </div>
+                <div>
+                    <div>
+                        <el-avatar size="large" :src="article.author.avatar"/>
+                    </div>
+                    <div>
+                        <div>
+                            <span>{{ article.author.username }}</span>
+                            <span>@{{ article.author.userId }}</span>
+                            <span>{{ article.author.level }}</span>
+                            <div>
+                                <el-button>Subscribe</el-button>
+                            </div>
+                        </div>
+                        <div>
+                            <span>{{ article.releaseTime }}</span>
+                            <span>{{ article.author.ipAddr }}</span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {{ article.content }}
+                </div>
             </div>
             <div class="flex flex-col">
                 comment
@@ -88,7 +61,7 @@ getOneArticle(articleId.value).then((res) => {
         </div>
         <div class="flex flex-col">
             <div>
-                user info
+                <UserInfo />
             </div>
             <div>
                 toc
