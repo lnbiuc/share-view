50e2ba7725dacc1e99fe9dff1a0a23facbc74a35
lnbiuc <15771670073@163.com>
2023-02-20 14:49:42 +0800

    fix: fix element icon unknown bug

--- src/components/index/articleList/CommentsLink.vue
+++ src/components/index/articleList/CommentsLink.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="px-2 flex flex-row items-center">
-        <el-icon color="gray"><Comment /></el-icon>
+        <el-icon color="gray"><ChatDotRound /></el-icon>
         <span class="mx-2 text-gray-500">{{ comments }}</span>
         <span class="hover:text-blue-500 transition-all text-gray-400 cursor-pointer">Comments</span>
     </div>
@@ -8,7 +8,7 @@
 
 <script setup lang="ts">
 // @ts-ignore
-import { Comment } from '@element-plus/icons-vue';
+import { ChatDotRound } from '@element-plus/icons-vue';
 
 const props = defineProps({
     comments: {
