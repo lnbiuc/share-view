9caf49ab98fd640d8e534897c37a207a7e3c0d20
lnbiuc <15771670073@163.com>
2023-03-14 23:45:35 +0800

    feat: add videos player

--- src/views/detail/VideoDetail.vue
+++ src/views/detail/VideoDetail.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
 
 <template>
-    <h1>vidoe</h1>
+    <div class="flex flex-col justify-center items-center">
+		<div class="flex mt-8">
+			<video-player
+				src="https://share-1308549476.cos.ap-shanghai.myqcloud.com/video/40931678807694021.mp4"
+				poster="https://vio.vin/card.png"
+				:controls="true"
+				:loop="true"
+				:volume="0.5"
+			/>
+		</div>
+	</div>
 </template>
