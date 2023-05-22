c44daebdcd7345233eb8c5fcc7f70f86f423f937
lnbiuc <15771670073@163.com>
2023-05-22 10:20:38 +0800

    fix: mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,8 @@ module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         screens: {
-            sm: '576px',
-            md: '992px',
+            sm: '580px',
+            md: '1000px',
             lg: '1200px',
             xl: '1440px',
         },
